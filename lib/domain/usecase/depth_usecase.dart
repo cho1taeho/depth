672fd427915d78416beacd31ad60635f51f48b6f
cho1taeho <cow12dog@gmail.com>
2025-06-21 21:50:16 +0900

    feat: photo save 변경

--- lib/domain/usecase/depth_usecase.dart
+++ lib/domain/usecase/depth_usecase.dart
@@ -1,43 +1,13 @@
+import 'package:camera/camera.dart';
 import 'package:depth/data/repository/depth_repository.dart';
-import 'package:depth/presentation/state/depth_state.dart';
+
 
 class DepthUseCase {
   final DepthRepository repository;
 
   DepthUseCase(this.repository);
 
-  Future<DepthState> takeDepthPicture() async {
-    try {
-      final filePath = await repository.takeDepthPicture();
-      return DepthState(
-        isLoading: false,
-        filePath: filePath,
-        error: null,
-      );
-    } catch (e) {
-      return DepthState(
-        isLoading: false,
-        filePath: null,
-        error: e.toString(),
-      );
-    }
+  Future<XFile> takePicture(CameraController controller) async {
+    return await repository.takePicture(controller);
   }
-
-  Future<DepthState> savePictureToLocal(String filePath, List<int> imageBytes) async {
-    try {
-      await repository.savePictureToLocal(filePath, imageBytes);
-      return DepthState(
-        isLoading: false,
-        filePath: filePath,
-        error: null,
-      );
-    } catch (e) {
-      return DepthState(
-        isLoading: false,
-        filePath: null,
-        error: e.toString(),
-      );
-    }
-  }
-
 }
